569bdbff23a64f52e1baeb295558a9dccd238f36
gabrielmsouza <gabrielmsouza@outlook.com.br>
2021-08-23 22:25:44 -0300

    DB H2

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,13 +1,42 @@
 package com.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.bookstore.domain.Categoria;
+import com.bookstore.domain.Livro;
+import com.bookstore.repositories.CategoriaRepository;
+import com.bookstore.repositories.LivroRepository;
 
+
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null,"Informatica", "Livros de TI");
+		
+		Livro l1 = new Livro(null,"Clean Code", "Joseph", "Lorem Ipsum", cat1);
+		
+		cat1.getLivros().add(l1);
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1)); 
 	}
 
 }
